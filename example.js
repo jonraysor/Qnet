4239c03d57ca28585c684b11ef68e9ae73454056
Anna Komarovska <ankakom3@gmail.com>
2019-04-06 18:11:45 -0400

    new changes

--- example.js
+++ example.js
@@ -1,5 +1,6 @@
   // Set the configuration for your app
   // TODO: Replace with your project's config object
+/*
   var config = {
     apiKey: "AIzaSyCGu-35PaXaoIXyxhZPHz_nDwZ5BHn0t44",
     authDomain: "pqnet-62c3e.firebaseapp.com",
@@ -12,3 +13,38 @@
   var database = firebase.database();
 
   console.log(database)
+
+
+  */
+  async function loadJson(url) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest()
+    xhr.overrideMimeType('application/json')
+    xhr.open('GET', url, true)
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.response)
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        })
+      }
+    }
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      })
+    }
+    xhr.send()
+  })
+}
+
+async function initDropdowns() {
+  let json = await loadJson('school.json')
+  let data = JSON.parse(json)
+  this.data = data
+  setSchoolsDropdown()
+  enableDropdown('schools')
+}
