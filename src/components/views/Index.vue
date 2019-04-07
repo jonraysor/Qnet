5007076016277e7cbceaf68c7523114246ad0f76
kevin <kevinwong1997@hotmail.com>
2019-04-07 03:52:09 -0400

    added search bar component

--- src/components/views/Index.vue
+++ src/components/views/Index.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="hello">
-
+  <div>
     <div class="mb-5 search-container">
-      <input id="schoolsBar"
+      <search-bar
+        placeholder="search the school"
+        :options="schools"
+        @update="pickedSchool = $event"
+        id="schoolsBar" class="mb-1"/>
+      <search-bar
+        placeholder="search the course"
+        :options="courses"
+        @update="pickedCourse = $event"
+        id="courseBar" class="mb-1"/>
+      <!-- <input id="schoolsBar"
         type="text"
         class="mb-1"
         placeholder="Search the school">
 			<input id="classBar"
         type="text"
         class="mb-1"
-        placeholder="Search the class">
+        placeholder="Search the class"> -->
       <div class="btn-bar">
-			  <img src="@/assets/search-icon.svg" onclick="alert(schoolsBar.value)">
+			  <img src="@/assets/search-icon.svg" @click="submitSearch()">
       </div>
     </div>
-
+    <!-- {{ pickedSchool }}<br>{{ pickedCourse }} -->
     <div class="text mb-3">
       <kbd>
         Find all your class quiz and test questions here.....
@@ -24,12 +33,36 @@
 </template>
 
 <script>
+import SearchBar from '@/components/SearchBar';
+
 export default {
   name: 'Index',
   data() {
     return {
+      schools: [],
+      courses: [],
+      pickedSchool: null,
+      pickedCourse: null,
     };
   },
+  components: {
+    SearchBar,
+  },
+  mounted() {
+    // 'http://10.255.139.136:5000'
+    let baseurl = '/api'
+    this.axios.get(`${baseurl}/readInSchools`).then((response) => {
+      this.schools = response.data
+    })
+    this.axios.get(`${baseurl}/readInCourses`).then((response) => {
+      this.courses = response.data
+    })
+  },
+  methods: {
+    submitSearch() {
+      alert(`${this.pickedSchool},${this.pickedCourse}`);
+    }
+  }
 };
 </script>
 
